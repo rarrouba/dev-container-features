18869a5cbfb9d52cccf5cae2d99ab704a02822a9
rarrouba <rarrouba@example.com>
2025-06-10 10:24:02 +0000

    ci: update pipeline as no scenartios exist yet

--- test/antidote/test.sh
+++ test/antidote/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'color' Feature with no options.
+# includes the 'antidote' Feature with no options.
 #
 # For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
 #
@@ -9,14 +9,14 @@
 # {
 #    "image": "<..some-base-image...>",
 #    "features": {
-#      "color": {}
+#      "antidote": {}
 #    },
 #    "remoteUser": "root"
 # }
 #
 # Thus, the value of all options will fall back to the default value in the
 # Feature's 'devcontainer-feature.json'.
-# For the 'color' feature, that means the default favorite color is 'red'.
+# For the 'antidote' feature, no options are available yet.
 #
 # These scripts are run as 'root' by default. Although that can be changed
 # with the '--remote-user' flag.
@@ -24,7 +24,7 @@
 # This test can be run with the following command:
 #
 #    devcontainer features test    \ 
-#               --features color   \
+#               --features antidote   \
 #               --remote-user root \
 #               --skip-scenarios   \
 #               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
