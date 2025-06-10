a83146f3c244b9dd2b668e268bf1d6f365154162
rarrouba <rarrouba@example.com>
2025-06-10 11:43:12 +0000

    fix(antidote): add wget using script

--- src/antidote/install.sh
+++ src/antidote/install.sh
@@ -3,7 +3,37 @@ set -e
 
 echo "Activating feature 'Antidote (zsh)'"
 
+# apt-get configuration
+export DEBIAN_FRONTEND=noninteractive
 
+dependencies(){
+
+    if command -v wget > /dev/null; then
+        return
+    fi
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+            echo "Detected Debian/Ubuntu-based system"
+            
+            apt-get update
+            apt-get install -y --no-install-recommends \
+                wget \
+                ca-certificates
+        elif [[ "$ID" == "centos" || "$ID" == "rhel" || "$ID" == "almalinux" ]]; then
+            echo "Detected RHEL/CentOS/AlmaLinux-based system"
+            dnf -y install wget
+        else
+            echo "Unsupported Linux distribution: $ID"
+            exit 1
+        fi
+    else
+        echo "Could not detect OS"
+        exit 1
+    fi
+}
+
+dependencies
 LATEST_VERSION=$(wget -qO- https://api.github.com/repos/mattmc3/antidote/releases/latest | grep -Po '"name": "\K[^"]+')
 DOWNLOAD_URL="https://github.com/mattmc3/antidote/archive/refs/tags/${LATEST_VERSION}.tar.gz"
 wget --no-verbose -O /tmp/${LATEST_VERSION}.tar.gz ${DOWNLOAD_URL}
@@ -26,3 +56,6 @@ if [ "$_CONTAINER_USER_HOME" != "$_REMOTE_USER_HOME" ]; then
     mkdir ${ZDOTDIR:-$_REMOTE_USER_HOME}/.antidote
     tar -xzf /tmp/${LATEST_VERSION}.tar.gz -C ${ZDOTDIR:-$_REMOTE_USER_HOME}/.antidote  --strip-components=1
 fi
+
+# Cleanup
+rm /tmp/${LATEST_VERSION}.tar.gz
